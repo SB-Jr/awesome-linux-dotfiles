34a8941b4fcd27b4b68361326254aa76d76eb2b4
SB-Jr <shrijitbasak@gmail.com>
2020-08-04 09:54:07 +0530

    Added i3-gaps dots

---
+++ bobomann18-i3-material_dark_rice/cmus/lib.pl
@@ -0,0 +1,11 @@
+/home/cesar/Musik/bad guy.mp3
+/home/cesar/Musik/Demons.mp3
+/home/cesar/Musik/Piece Of Your Heart.mp3
+/home/cesar/Musik/lonely day - lofi hiphop mix.mp3
+/home/cesar/Musik/Pop Out.mp3
+/home/cesar/Musik/21.mp3
+/home/cesar/Musik/Roses.mp3
+/home/cesar/Musik/Closer.mp3
+/home/cesar/Musik/Sick Boy.mp3
+/home/cesar/Musik/You Owe Me.mp3
+/home/cesar/Musik/Everybody Hates Me.mp3
---
+++ bobomann18-i3-material_dark_rice/cmus/playlist.pl
@@ -0,0 +1,3 @@
+/home/cesar/Musik/Sick Boy.mp3
+/home/cesar/Musik/Closer.mp3
+/home/cesar/Musik/Pop Out.mp3
